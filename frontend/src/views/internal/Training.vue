d0d2bc4ca5e14af8106da1d5c5f35bdb6b1d8220
Alexey Samoluk <aleshasam@mail.ru>
2024-06-06 13:55:15 +0700

    Edit training template

--- frontend/src/views/internal/Training.vue
+++ frontend/src/views/internal/Training.vue
@@ -2,10 +2,16 @@
   <section>
     <div id="exercises-list" class="w-full overflow-x-auto">
       <div class="overflow-hidden min-w-max">
-        <div class="grid grid-cols-2 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
-          <div class="text-gray-500 dark:text-gray-400">exercise 1</div>
-          <div class="text-gray-500 dark:text-gray-400">
-            counts
+        <div>
+          <h2 class="text-gray-500 dark:text-gray-400">Тренировка</h2>
+          <div class="grid grid-cols-2 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+            <div class="text-gray-500 dark:text-gray-400 col-span-2">Упражнение</div>
+            <div class="text-gray-500 dark:text-gray-400">
+              counts
+            </div>
+            <div class="text-gray-500 dark:text-gray-400 col-span-2">
+              counts
+            </div>
           </div>
         </div>
       </div>
